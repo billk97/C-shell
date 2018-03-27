fe7e9102c3ef42484018fae33a4247f5582c501d
Bill <bill1897@yahoo.gr>
2018-03-27 17:33:12 +0300

    samgoodchanjes

--- lab2/c/ergLeitourgika/Strtest.c
+++ lab2/c/ergLeitourgika/Strtest.c
@@ -6,14 +6,13 @@ int main( ) {
   printf(">");
   scanf("%s\n", InputBuffer);
   while (InputBuffer[i]!='\0') {
-    printf("Buffer: %c\n",InputBuffer[i] );
+    printf("Buffer: %c\n",InputBuffer[i]);
     c[i]=InputBuffer[i];
-
     printf("c: %c\n",c[i] );
     printf("i: %d\n",i );
       i++;
   }
+  printf("c: %s\n",c );
   return 0;
-
 }
 /*na dv gt den mporo na ballo ets times sto c*/
