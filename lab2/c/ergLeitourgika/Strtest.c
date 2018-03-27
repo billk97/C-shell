7e435ad6156815ad98fa07c55d6c0e5f28aaebef
Bill <bill1897@yahoo.gr>
2018-03-27 20:28:27 +0300

    dokimi1

--- lab2/c/ergLeitourgika/Strtest.c
+++ lab2/c/ergLeitourgika/Strtest.c
@@ -2,17 +2,30 @@
 
 int main( ) {
   char InputBuffer [255] ,c [255];
-  int i=0;
+  int i=0,j=0;
   printf(">");
-  scanf("%s\n", InputBuffer);
+      scanf("%s\n", InputBuffer);
+      printf("InputBuffer: %s\n",InputBuffer);
   while (InputBuffer[i]!='\0') {
-    printf("Buffer: %c\n",InputBuffer[i]);
+  //  printf("Buffer: %c\n",InputBuffer[i]);
     c[i]=InputBuffer[i];
     printf("c: %c\n",c[i] );
     printf("i: %d\n",i );
       i++;
   }
+  printf("1 c: %s\n",c );
+  scanf("%s\n", InputBuffer);
+  printf("2 InputBuffer: %s\n",InputBuffer );
+  while (InputBuffer[i]!='\0') {
+    printf("Buffer: %c\n",InputBuffer[i]);
+    c[i]=InputBuffer[i];
+    printf("2 c: %c\n",c[i] );
+    printf("i: %d\n",i );
+      i++;
+  }
   printf("c: %s\n",c );
+
+
   return 0;
 }
 /*na dv gt den mporo na ballo ets times sto c*/
