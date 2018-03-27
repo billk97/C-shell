7a194147fa51ae52f55a1e9ebe221f34de23d051
Bill <bill1897@yahoo.gr>
2018-03-27 14:27:56 +0300

    input

---
+++ lab2/c/ergLeitourgika/input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main (  int argc, char const *argv[])
+{
+  int i;
+  char eisodos [255]="gia me lene basilh";
+  const char my = " ";
+  for(i=0;i<255;i++ ||eisodos[i]=='\0')
+  {
+    if(eisodos[i]==' ')
+    {
+      printf("o look a space: ");
+      printf("i: %d\n",i );
+    }
+  }
+
+}
