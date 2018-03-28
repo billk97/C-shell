ed181b0d375fbfa3ce67e41ca036b7fa3ee83714
Bill <bill1897@yahoo.gr>
2018-03-28 18:43:51 +0300

    ok

--- lab2/c/ergLeitourgika/testingfile.c
+++ lab2/c/ergLeitourgika/testingfile.c
@@ -2,17 +2,28 @@
 
 int main(int argc, char const *argv[])
 {
-  int c;
+  char c;
   printf("Enter a value:");
-  c=getchar();
+  if ( scanf( " %c", &c ) == EOF ) {
+      printf( "eof break" );
+      return 0;
+  }
   while(c!=EOF)
   {
     printf("\n the loop begins hear");
     printf("\n the value is: ");
-    putchar(c);
+    if ( c == '\n' )
+    {
+          printf( ":O look c is equal to \\n"  );
+    }
+    putchar( c );
     printf("\n Enter a value:");
     c=getchar();
+    if ( scanf( " %c", &c ) == EOF )
+    {
+        printf("eof break\n" );
+        break;
+    }
   }
-
   return 0;
 }
